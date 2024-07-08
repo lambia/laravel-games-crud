e60eab3362fc04f16f763f82d56b03c6819a63bc
Luca <luca@lambia.it>
2024-07-08 12:22:23 +0200

    fix some links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -20,7 +20,7 @@
                         <a class="nav-link" href="{{ route('games.index') }}">Catalogo</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/quale/rotta/chiamo?">Aggiungi un gioco</a>
+                        <a class="nav-link" href="{{ route('games.create') }}">Aggiungi un gioco</a>
                     </li>
                 </ul>
             </div>
