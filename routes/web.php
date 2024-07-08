87d0cd0f043d5d3753da235913be9540aac31d5e
Luca <luca@lambia.it>
2024-07-08 10:21:48 +0200

    add comments

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,12 @@ Route::get('/', [PageController::class, "index"])->name("home");
 Route::get('/about', [PageController::class, "about"])->name("about");
 
 Route::resource('/games', GameController::class);
+
+// Le stesse rotte di Route::resource, create manualmente:
+// Route::get('/games', [GameController::class, "index"])->name("games.index");
+// Route::post('/games', [GameController::class, "store"])->name("games.store");
+// Route::get('/games/create', [GameController::class, "create"])->name("games.create");
+// Route::get('/games/{gioco}', [GameController::class, "show"])->name("games.show");
+// Route::put('/games/{gioco}', [GameController::class, "update"])->name("games.update");
+// Route::delete('/games/{gioco}', [GameController::class, "destroy"])->name("games.destroy");
+// Route::get('/games/{gioco}/edit', [GameController::class, "edit"])->name("games.edit");
